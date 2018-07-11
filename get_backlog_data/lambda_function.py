d6e93067d14498e159d4654df4c1b93602cdb1a1
3panda <3panda@gmail.com>
2018-07-11 19:30:18 +0900

    Frontに必要な情報を渡す処理調整

--- get_backlog_data/lambda_function.py
+++ get_backlog_data/lambda_function.py
@@ -19,49 +19,66 @@ def lambda_handler(event, context):
         print(API_KEY)
 
         r = get_users_info(HOST, API_KEY)
-        MY_ID = r.json()['id']
+        my_id = r.json()['id']
 
-        print(MY_ID)
+        print(my_id)
 
         # プロジェクト情報取得
         # ${HOST}/api/v2/projects/${PROJECT_ID}?${API_KEY}
         r = requests.get(HOST + '/api/v2/projects/' + PROJECT_KEY + '?' + API_KEY)
-        PROJECT_ID = r.json()['id']
+        project_id = r.json()['id']
         print(r.json())
 
         # 種別一覧の取得
         # ${HOST}/api/v2/projects/${PROJECT_ID}/issueTypes?${API_KEY}
         r = requests.get(HOST + '/api/v2/projects/' + PROJECT_KEY + '/issueTypes?' + API_KEY)
         print(r.json())
-        ISSUE_TYPE_ID = r.json()[0]['id']
+        issue_type_id = r.json()[0]['id']
 
         # 優先度一覧の取得
         # ${HOST}/api/v2/priorities?${API_KEY}
         r = requests.get(HOST + '/api/v2/priorities?' + API_KEY)
         print(r.json())
 
-        PRIORITY_ID = r.json()[0]['id']
+        priority_id = r.json()[0]['id']
 
         # 課題一覧
         print("------------")
         # ${HOST}/api/v2/issues/${PROJECT_ID}/issueTypes?${API_KEY}
         params = {
-            'assigneeId[]': MY_ID,
+            'assigneeId[]': my_id,
             'statusId[]': 1
         }
         r = requests.get(HOST + '/api/v2/issues?' + API_KEY, params=params)
 
         issues_data = r.json()
+        print('----')
         print(issues_data)
 
+        issues_list = []
+
+        for issue in issues_data:
+            issues_list.append({
+                'id': issue['id'],
+                'summary': issue['summary'],
+                'description': issue['description']
+            })
+        print(issues_list)
 
         return {
             'statusCode': 200,
             'headers': {
-                    'access-control-allow-origin': '*',
-                    'content-type': 'application/json'
+                'access-control-allow-origin': '*',
+                'content-type': 'application/json'
             },
-            'data': issues_data,
+            # 'data': issues_data,
+            'project_info': {
+                'project_id': project_id,
+                'my_id': my_id,
+                'issue_type_id': issue_type_id,
+                'priority_id': priority_id,
+                'issues': issues_list
+            },
             'completed': 1
 
         }
@@ -72,8 +89,8 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                    'access-control-allow-origin': '*',
-                    'content-type': 'application/json'
+                'access-control-allow-origin': '*',
+                'content-type': 'application/json'
             },
             'completed': 0
         }
